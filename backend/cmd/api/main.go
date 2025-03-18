4a0432779750422af3edf6a5ff723c4bbc57621a
aybancaughtin4k <203548283+aybancaughtin4k@users.noreply.github.com>
2025-03-18 11:38:17 +0800

    feat: add server related functionalities

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/aybancaughtin4k/gymops/backend/internal/data"
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type config struct {
+	port int
+	env  string
+	dsn  string
+}
+
+type application struct {
+	logger *slog.Logger
+	config config
+	dbpool *pgxpool.Pool
+	models data.Models
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "The port of server that will use")
+	flag.StringVar(&cfg.env, "environment", "development", "Server environment (production|development)")
+	flag.StringVar(&cfg.dsn, "dsn", os.Getenv("GOOSE_DBSTRING"), "Database dsn")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+
+	dbpool, err := initDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("initializing database connection")
+
+	defer dbpool.Close()
+
+	app := &application{
+		logger: logger,
+		config: cfg,
+		dbpool: dbpool,
+		models: data.NewModels(dbpool),
+	}
+
+	err = app.serve()
+	if err != nil {
+		app.logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func initDB(cfg config) (*pgxpool.Pool, error) {
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("GOOSE_DBSTRING"))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = dbpool.Ping(ctx)
+	if err != nil {
+		dbpool.Close()
+		return nil, err
+	}
+
+	return dbpool, nil
+}
---
+++ backend/cmd/api/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// Server api routes
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		app.notFoundResponse(w, r)
+	})
+
+	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		app.methodNotAllowedResponse(w, r)
+	})
+
+	r.Get("/api/v1/healthcheck", app.healthCheckHandler)
+
+	r.Post("/api/v1/users/register", app.registerHandler)
+
+	return r
+}
+
+// Health check handler
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	app.writeJSON(w, http.StatusOK, envelope{"status": "OK"}, nil)
+}
